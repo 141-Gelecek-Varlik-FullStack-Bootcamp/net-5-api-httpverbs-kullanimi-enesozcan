3bde8826047249f8681e52e90c79d7fbf592f898
Enes Gökhan Özcan <enes.gokhan.ozcan@gmail.com>
2021-11-26 15:30:34 +0300

    model called  from a different project, and data annotations, data validation added

---
+++ Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    //Data Annotations
+    public class User
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your surname.")]
+        public string UserSurname { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email.")]
+        public string UserEmail { get; set; }
+
+    }
+}
